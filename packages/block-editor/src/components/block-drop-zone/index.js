bce23fc95385e4062cdf26c56c71eb68250cdb5a
Jorge <jorge.costa@developer.pt>
2019-04-18 17:58:07 +0100

    Fix: Impossible to drag & drop blocks if the user has no permission to upload files

--- packages/block-editor/src/components/block-drop-zone/index.js
+++ packages/block-editor/src/components/block-drop-zone/index.js
@@ -19,11 +19,6 @@ import { Component } from '@wordpress/element';
 import { withDispatch, withSelect } from '@wordpress/data';
 import { compose } from '@wordpress/compose';
 
-/**
- * Internal dependencies
- */
-import MediaUploadCheck from '../media-upload/check';
-
 const parseDropEvent = ( event ) => {
 	let result = {
 		srcRootClientId: null,
@@ -111,25 +106,21 @@ class BlockDropZone extends Component {
 	}
 
 	render() {
-		const { isLockedAll } = this.props;
+		const { hasUploadPermissions, isLockedAll } = this.props;
 		if ( isLockedAll ) {
 			return null;
 		}
-
 		const index = this.getInsertIndex();
 		const isAppender = index === undefined;
-
 		return (
-			<MediaUploadCheck>
-				<DropZone
-					className={ classnames( 'editor-block-drop-zone block-editor-block-drop-zone', {
-						'is-appender': isAppender,
-					} ) }
-					onFilesDrop={ this.onFilesDrop }
-					onHTMLDrop={ this.onHTMLDrop }
-					onDrop={ this.onDrop }
-				/>
-			</MediaUploadCheck>
+			<DropZone
+				className={ classnames( 'editor-block-drop-zone block-editor-block-drop-zone', {
+					'is-appender': isAppender,
+				} ) }
+				onHTMLDrop={ this.onHTMLDrop }
+				onDrop={ this.onDrop }
+				onFilesDrop={ hasUploadPermissions ? this.onFilesDrop : undefined }
+			/>
 		);
 	}
 }
@@ -158,11 +149,17 @@ export default compose(
 		};
 	} ),
 	withSelect( ( select, { rootClientId } ) => {
-		const { getClientIdsOfDescendants, getTemplateLock, getBlockIndex } = select( 'core/block-editor' );
-		return {
-			isLockedAll: getTemplateLock( rootClientId ) === 'all',
-			getClientIdsOfDescendants,
+		const {
 			getBlockIndex,
+			getClientIdsOfDescendants,
+			getSettings,
+			getTemplateLock,
+		} = select( 'core/block-editor' );
+		return {
+			getBlockIndex,
+			getClientIdsOfDescendants,
+			hasUploadPermissions: !! getSettings().__experimentalMediaUpload,
+			isLockedAll: getTemplateLock( rootClientId ) === 'all',
 		};
 	} ),
 	withFilters( 'editor.BlockDropZone' )
